1a9ee97286bdeb7677b4d52cf24bdc0c52243942
Anansi86 <s.albert.staton@gmail.com>
2022-09-09 15:35:21 +0000

    debugging

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function randomValueFromArray(array){
 
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 
-let insertX = ["Willy the Goblin, Big Daddy, Father Christmas",
+let insertX = ["Willy the Goblin",
                "Big Daddy",
                "Father Christmas"];
 
@@ -35,9 +35,9 @@ function result() {
     let xItem = randomValueFromArray(insertX);
     let yItem = randomValueFromArray(insertY);
     let zItem = randomValueFromArray(insertZ);
-    newStory= newStory.replace('insertx'xItem);
-    newStory= newStory.replace('inserty'yItem);
-    newStory= newStory.replace('insertz'zItem);
+    newStory= newStory.replace(':insertx:', xItem);
+    newStory= newStory.replace(':inserty:', yItem);
+    newStory= newStory.replace(':insertz:', zItem);
 
 
   if(customName.value !== '') {
