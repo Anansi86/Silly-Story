4abba29b654d2ea381beb7bce214334287d18146
Anansi86 <s.albert.staton@gmail.com>
2022-09-09 14:46:01 +0000

    replace placeholders

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+//1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
 
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
@@ -9,7 +9,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-2. RAW TEXT STRINGS
+// 2. RAW TEXT STRINGS
 
 let storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day."
 
@@ -25,15 +25,24 @@ let insertZ = ["spontaneously combusted",
                "melted into a puddle on the sidewalk",
                "turned into a slug and crawled away"];
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+//3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
+
 function result() {
+    let newStory = storyText;
+    let xItem = randomValueFromArray(insertX);
+    let yItem = randomValueFromArray(insertY);
+    let zItem = randomValueFromArray(insertZ);
+    newStory= newStory.replace('insertx'xItem);
+    newStory= newStory.replace('inserty'yItem);
+    newStory= newStory.replace('insertz'zItem);
+
 
   if(customName.value !== '') {
     const name = customName.value;
-
+    
   }
 
   if(document.getElementById("uk").checked) {
